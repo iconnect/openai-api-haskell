5e6c74d702a0243bf85ac4f225e72eba492518ac
Alfredo Di Napoli <alfredo@well-typed.com>
2024-09-05 15:36:56 +0200

    Disable integration tests in CI

--- openai-api/test/Spec.hs
+++ openai-api/test/Spec.hs
@@ -1,10 +1,16 @@
 import ApiSpec
+import Control.Monad
 import HelperSpec
+import System.Environment
 import Test.Hspec
 
+isCI :: IO Bool
+isCI = maybe False (const True) <$> lookupEnv "CI"
+
 main :: IO ()
-main =
+main = do
+  runningInCI <- isCI
   hspec $
     do
-      apiSpec
+      unless runningInCI apiSpec
       helperSpec
